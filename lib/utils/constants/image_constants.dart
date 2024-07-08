91c26f487b38e7543bc9c79f35320763528e108c
Amrutha <16-5581@users.noreply.gitlab.maxvalue.co.in>
2024-07-08 10:45:39 +0530

    User Screen partially completed

--- lib/utils/constants/image_constants.dart
+++ lib/utils/constants/image_constants.dart
@@ -2,5 +2,5 @@ class imageConstants
 {
   static const String LOGO_PNG = "assets/images/splashbgimage.png";
   static const String USER_PNG = "assets/images/user.png";
-  static const String KIDSUSER_PNG = "assets/images/kids.png";
+  static const String KIDSUSER_PNG = "assets/images/star.png"; // amazonkids
 }
--- lib/view/username_screen/UserNameScreen.dart
+++ lib/view/username_screen/UserNameScreen.dart
@@ -21,61 +21,100 @@ class _UserNameScreenState extends State<UserNameScreen>
     return Scaffold(
       backgroundColor: Colors.black,
       body: Center(
-        child: GridView.builder(
-          padding: EdgeInsets.symmetric(horizontal: 50),
-          shrinkWrap: true,
-            itemCount: DummyDB.usersList.length+1,
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisSpacing: 10,
-                crossAxisCount: 2,
-                mainAxisSpacing: 10,
-                mainAxisExtent: 120),
-            itemBuilder: (context, index) {
-              if(index < DummyDB.usersList.length)
-              {
-                return InkWell(
-                  onTap: (){
-                    Navigator.push(context, MaterialPageRoute(builder: (context) => HomeScreen()));
-                  },
-                  child: Column(
-                    children: [
-                      Image.asset(DummyDB.usersList[index]["imagePath"]!),
-                      SizedBox(height: 5,),
-                      Text(DummyDB.usersList[index]["userName"]!,
-                        style: TextStyle(fontSize: 13.25, color: colorConstants.mainwhite),)
-                    ],
-                  ),
-                );
-              }
-              else
-              {
-                return InkWell(
-                  onTap: (){
-                    DummyDB.usersList.add(
-                      {"imagePath": imageConstants.USER_PNG, "userName": "sarovar"},
-                    );
-                    setState(() {});
-                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                        backgroundColor: Colors.yellow,
-                        content: Text("Profile added successfully")));
-                  },
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      CircleAvatar(
-                        backgroundColor: Colors.grey.shade600,
-                        radius: 40,
-                        child: Icon(Icons.add, color: Colors.white,),
+        child: Column(
+          //crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              child: Text("Who's watching?", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 16),),
+            ),
+            SizedBox(height: 30,),
+            GridView.builder(
+                padding: EdgeInsets.symmetric(horizontal: 50),
+                shrinkWrap: true,
+                itemCount: DummyDB.usersList.length+1,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisSpacing: 10,
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 10,
+                    mainAxisExtent: 120),
+                itemBuilder: (context, index) {
+                  if(index < DummyDB.usersList.length)
+                  {
+                    return InkWell(
+                      onTap: (){
+                        Navigator.push(context, MaterialPageRoute(builder: (context) => HomeScreen()));
+                      },
+                      child: Column(
+                        children: [
+                          Image.asset(DummyDB.usersList[index]["imagePath"]!),
+                          SizedBox(height: 5,),
+                          Text(DummyDB.usersList[index]["userName"]!,
+                            style: TextStyle(fontSize: 13.25, color: colorConstants.mainwhite),)
+                        ],
                       ),
-                      Text("New", style: TextStyle(
-                        color: colorConstants.mainwhite,
-                        fontSize: 13.25,
-                      ),),
-                    ],
-                  ),
-                );
-              }
-            }),
+                    );
+                  }
+                  else
+                  {
+                    return InkWell(
+                      onTap: (){
+                        DummyDB.usersList.add(
+                          {"imagePath": imageConstants.USER_PNG, "userName": "sarovar"},
+                        );
+                        setState(() {});
+                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                            backgroundColor: Colors.yellow,
+                            content: Text("Profile added successfully")));
+                      },
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          CircleAvatar(
+                            backgroundColor: Colors.grey,
+                            radius: 40,
+                            child: Icon(Icons.add, color: Colors.white,),
+                          ),
+                          Text("New", style: TextStyle(
+                            color: colorConstants.mainwhite,
+                            fontSize: 13.25,
+                          ),),
+                        ],
+                      ),
+                    );
+                  }
+                }
+                ),
+          //Spacer(),
+            SizedBox(height: 150,),
+          Container(
+           width: 300,
+           height: 50,
+           child: ElevatedButton(
+           onPressed: () {},
+           style: ElevatedButton.styleFrom(
+            backgroundColor: colorConstants.mainwhite,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(5),
+            ),
+            elevation: 15.0,
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Text(
+              'Edit profiles',
+              style: TextStyle(fontSize: 16, color: colorConstants.mainBlack),
+            ),
+          ),
+        ),
+              ),
+
+            SizedBox(height: 10,),
+
+            Text("Learn more", style: TextStyle(fontSize: 16, color: colorConstants.mainwhite, decoration: TextDecoration.underline, decorationColor: colorConstants.mainwhite, decorationThickness: 2, fontWeight: FontWeight.bold),)
+
+          ],
+        ),
       ),
     );
   }
